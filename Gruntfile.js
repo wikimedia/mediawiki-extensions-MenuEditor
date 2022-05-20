cd021ab8f0ae9298483150ce4a32683a96c061c3
rvogel <vogel@hallowelt.biz>
2022-05-20 16:32:01 +0200

    Update CI files
    Change-Id: Idf6e73398bb3dde7af427b04acd8f96809df525e

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,30 @@
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.initConfig( {
-		banana: conf.MessagesDirs,
-		jsonlint: {
-			all: [
-				'**/*.json',
+		eslint: {
+			options: {
+				cache: true
+			},
+			src: [
+				'**/*.{js,json}',
 				'!node_modules/**',
-				'!vendor/**'
+				'!vendor/**',
 			]
-		}
+		},
+		// eslint-disable-next-line es/no-object-assign
+		banana: Object.assign(
+			conf.MessagesDirs,
+			{
+				options: {
+					requireLowerCase: 'initial'
+				}
+			}
+		)
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
