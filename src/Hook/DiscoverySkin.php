7be7045995fbef76af5670b8ceea17d44c1e3ee5
miriam schlindwein <schlindwein@hallowelt.com>
2023-02-21 09:03:22 +0100

    Fix actions for menu edit
    ERM31520
    
    [4.2.x]
    
    Change-Id: I51e3f4289b149224d88e446cfd05049a62d33cb0

--- src/Hook/DiscoverySkin.php
+++ src/Hook/DiscoverySkin.php
@@ -4,13 +4,39 @@ namespace MediaWiki\Extension\MenuEditor\Hook;
 
 use BlueSpice\Discovery\Hook\BlueSpiceDiscoveryTemplateDataProviderAfterInit;
 use BlueSpice\Discovery\ITemplateDataProvider;
+use MediaWiki\Extension\MenuEditor\MenuFactory;
+use RequestContext;
 
 class DiscoverySkin implements BlueSpiceDiscoveryTemplateDataProviderAfterInit {
+
+	/** @var MenuFactory */
+	private $menuFactory;
+
+	/**
+	 * @param MenuFactory $menuFactory
+	 */
+	public function __construct( MenuFactory $menuFactory ) {
+		$this->menuFactory = $menuFactory;
+	}
 
 	/**
 	 * @param ITemplateDataProvider $registry
 	 */
 	public function onBlueSpiceDiscoveryTemplateDataProviderAfterInit( $registry ): void {
+		$context = RequestContext::getMain();
+		$title = $context->getTitle();
+		$menus = $this->menuFactory->getAllMenus();
+		$applied = false;
+
+		foreach ( $menus as $key => $menu ) {
+			if ( $menu->appliesToTitle( $title ) ) {
+				$applied = true;
+			}
+		}
+		if ( !$applied ) {
+			return;
+		}
+
 		$registry->register( 'panel/edit', 'ca-menueditsource' );
 		$registry->unregister( 'panel/edit', 'ca-new-section' );
 		$registry->unregister( 'panel/edit', 'ca-ve-edit' );
