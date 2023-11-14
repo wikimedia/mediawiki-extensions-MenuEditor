6b4348f737f0c5d4ada3f4a4397fda7d7a5071d2
hslater <hamish.slater@hallowelt.com>
2023-11-14 22:24:26 +1300

    Improve FooterLinks MenuEditor
    Display a banner
    
    ERM33411
    
    [REL1_39, REL1_39-2.0.x, master]
    
    Change-Id: If2438e7a744fd166aa8a9b8504244536dafc8870

---
+++ resources/ext.menuEditor.footerlinks.banner.js
@@ -0,0 +1,12 @@
+( function ( $, mw ) {
+	var $content = $( '#content' ), // eslint-disable-line no-jquery/no-global-selector
+
+		widget = new OO.ui.MessageWidget( {
+			id: 'footerlinks-notice',
+			type: 'notice',
+			label: mw.message( 'menueditor-footerlinks-banner-text' ).plain()
+		} );
+
+	widget.$element.css( 'margin-bottom', '20px' );
+	$content.prepend( widget.$element );
+}( jQuery, mediaWiki ) );
