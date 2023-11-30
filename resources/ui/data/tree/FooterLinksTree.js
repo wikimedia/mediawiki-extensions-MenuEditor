780a35fbea32814c6072b227bd73acfbbd8a2ebf
hslater <hamish.slater@hallowelt.com>
2023-12-01 02:39:35 +1300

    eslint
    Align with REL1_39
    
    [REL1_39-2.0.x]
    
    Change-Id: I31c676b0568df60fb65bb07830531b08b0281b63

--- resources/ui/data/tree/FooterLinksTree.js
+++ resources/ui/data/tree/FooterLinksTree.js
@@ -2,10 +2,8 @@ ext.menueditor.ui.data.tree.FooterLinksTree = function ( cfg ) {
 	ext.menueditor.ui.data.tree.FooterLinksTree.parent.call( this, cfg );
 };
 
-// eslint-disable-next-line max-len
 OO.inheritClass( ext.menueditor.ui.data.tree.FooterLinksTree, ext.menueditor.ui.data.tree.Tree );
 
-// eslint-disable-next-line max-len
 ext.menueditor.ui.data.tree.FooterLinksTree.prototype.getPossibleNodesForLevel = function ( lvl ) {
 	switch ( lvl ) {
 		case 0:
