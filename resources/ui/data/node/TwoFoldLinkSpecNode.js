1d3db8ce361788aee509e9b1878ecf61743d7e51
Mschlindwein <schlindwein@hallowelt.com>
2025-01-22 07:05:42 +0100

    Fix dialog overlay
    ERM40091
    
    [5.x]
    
    Change-Id: I52233ee9b810f412a8edf0d51a34e25ab8e9edb6

--- resources/ui/data/node/TwoFoldLinkSpecNode.js
+++ resources/ui/data/node/TwoFoldLinkSpecNode.js
@@ -23,6 +23,8 @@ ext.menueditor.ui.data.node.TwoFoldLinkSpecNode.prototype.getFormFields = functi
 			name: 'target',
 			type: 'title',
 			required: true,
+			// eslint-disable-next-line camelcase
+			widget_$overlay: dialog.$overlay,
 			label: mw.message( 'menueditor-ui-form-field-target' ).text(),
 			help: new OO.ui.HtmlSnippet( mw.message( 'menueditor-ui-menu-two-fold-link-spec-help' ).text() )
 		},
