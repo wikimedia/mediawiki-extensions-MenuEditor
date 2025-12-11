bfeb9767cab2234c598cc22ebaef209c26e37bc6
hslater <hamish.slater@hallowelt.com>
2025-12-11 22:10:18 +0700

    Restrict applicable content models
    Prevents adding menueditsource action in books
    
    ERM45355
    
    [5.1.x]
    
    Change-Id: I2ac0bdf28aebd1601081229600a46cb442763953

--- src/Hook/EditActionHookHandler.php
+++ src/Hook/EditActionHookHandler.php
@@ -124,7 +124,7 @@ class EditActionHookHandler implements
 	 * @param array &$links
 	 */
 	public function onSkinTemplateNavigation__Universal( $sktemplate, &$links ): void {
-		if ( !$this->title ) {
+		if ( $this->shouldSkipProcessing( $sktemplate ) ) {
 			return;
 		}
 
@@ -148,4 +148,25 @@ class EditActionHookHandler implements
 			];
 		}
 	}
+
+	/**
+	 * @param SkinTemplate $sktemplate
+	 * @return bool
+	 */
+	protected function shouldSkipProcessing( SkinTemplate $sktemplate ) {
+		if ( !$this->title ) {
+			return true;
+		}
+
+		$allowedModels = [
+			CONTENT_MODEL_WIKITEXT,
+			CONTENT_MODEL_JSON,
+		];
+
+		return !in_array(
+			$this->title->getContentModel(),
+			$allowedModels
+		);
+	}
+
 }
