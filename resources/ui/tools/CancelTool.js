050f71a43d54235f4c8281f66f16dce40ce0232d
Mschlindwein <schlindwein@hallowelt.com>
2026-09-23 11:28:04 +0200

    Add accessible label to cancel button
    Fix position of expander
    
    ERM50356
    
    [5.x]
    
    Change-Id: Iaabaeb7396c1dee6d11de6a3752a323542f706c2

--- resources/ui/tools/CancelTool.js
+++ resources/ui/tools/CancelTool.js
@@ -1,5 +1,7 @@
 ext.menueditor.ui.tools.CancelTool = function () {
 	ext.menueditor.ui.tools.CancelTool.super.apply( this, arguments );
+	// Link has no related text - ERM50356
+	this.$link.attr( 'aria-label', mw.message( 'menueditor-toolbar-cancel' ) );
 };
 OO.inheritClass( ext.menueditor.ui.tools.CancelTool, OO.ui.Tool );
 ext.menueditor.ui.tools.CancelTool.static.name = 'cancel';
